6f4a8ce83854170c3b25f9dcef74722395a33030
Alex-GHP <alex.morariu.dev@gmail.com>
2024-06-10 09:37:02 +0300

    tree: bst insert

--- Trees/BST.py
+++ Trees/BST.py
@@ -4,4 +4,22 @@ class BSTNode:
         self.right = None
         self.val = val
     
-    
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)       
