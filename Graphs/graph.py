4a78248b5e049625d88733109b9bf61e47642d6c
Alex-GHP <alex.morariu.dev@gmail.com>
2024-06-27 09:44:31 +0300

    graph: edge exists

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -25,3 +25,9 @@ class Graph:
                 unconnected.append(key)
         return unconnected
     
+
+    def edge_exists(self, u, v):
+        if u in self.graph and v in self.graph:
+            return (v in self.graph[u]) and (u in self.graph[v])
+        return False
+    
